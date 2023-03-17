3e2b11665ca281da541754098f96dc79495118bf
Dylan <mill1429@mail.nmc.edu>
2023-03-17 01:36:00 -0400

    init

--- Assignment3Week3/Program.cs
+++ Assignment3Week3/Program.cs
@@ -1,66 +1,50 @@
 ﻿using System;
 
-namespace assignment8ex1
+namespace Assignment8ex2
 {
-    class Program
+    public class MathSolutions
     {
-        public delegate void SongDelegate();
-
-        public class HereComesTheSun
+        public double GetSum(double x, double y)
         {
-            public static void chorus()
-            {
-                Console.Write("Here comes the sun, doo-doo-doo-doo\r\n");
-                Console.WriteLine("Here comes the sun, and I say\r\nIt's alright");
-                Console.WriteLine();
-            }
-            public static void chorus2()
-            {
-                Console.WriteLine("Here comes the sun\r\nHere comes the sun, and I say\r\nIt's alright");
-                Console.WriteLine();
-                Console.Write("Sun, sun, sun, here it comes\r\nSun, sun, sun, here it comes\r\n");
-                Console.Write("Sun, sun, sun, here it comes\r\nSun, sun, sun, here it comes\r\n");
-                Console.WriteLine("Sun, sun, sun, here it comes");
-                Console.WriteLine();
-            }
-            public static void verse1()
-            {
-                Console.Write("Little darlin', it's been a long, cold, lonely winter\r\n");
-                Console.WriteLine("Little darlin', it feels like years since it's been here");
-                Console.WriteLine();
-            }
-            public static void verse2()
-            {
-                Console.Write("Little darlin', the smile's returning to their faces\r\n");
-                Console.WriteLine("Little darlin', it seems like years since it's been here");
-                Console.WriteLine();
-            }
-            public static void verse3()
-            {
-                Console.Write("Little darlin', I feel that ice is slowly melting\r\n");
-                Console.WriteLine("Little darlin', it seems like years since it's been clear");
-                Console.WriteLine();
-            }
-            public static void end()
-            {
-                Console.WriteLine("It's alright");
-            }
+            return x + y;
         }
+        public double GetProduct(double a, double b)
+        {
+            return a * b;
+        }
+        public void GetSmaller(double a, double b)
+        {
+            if (a < b)
+                Console.WriteLine($" {a} is smaller than {b}");
+            else if (b < a)
+                Console.WriteLine($" {b} is smaller than {a}");
+            else
+                Console.WriteLine($" {b} is equal to {a}");
+        }
+
+        //custom delegate for GetProduct
+        public delegate double ProductDelegate(double a, double b);
 
         static void Main(string[] args)
         {
-            SongDelegate song = HereComesTheSun.chorus;
+            MathSolutions answer = new MathSolutions();
+            Random r = new Random();
+            double num1 = Math.Round(r.NextDouble() * 100);
+            double num2 = Math.Round(r.NextDouble() * 100);
 
-            song += HereComesTheSun.verse1;
-            song += HereComesTheSun.chorus;
-            song += HereComesTheSun.verse2;
-            song += HereComesTheSun.chorus2;
-            song += HereComesTheSun.verse3;
-            song += HereComesTheSun.chorus;
-            song += HereComesTheSun.chorus;
-            song += HereComesTheSun.end;
+            //func delegate for GetSum
+            Func<double, double, double> sumFunc = answer.GetSum;
+            double sumResult = sumFunc(num1, num2);
+            Console.WriteLine($" {num1} + {num2} = {sumResult}");
 
-            song();
+            //custom delegate for GetProduct
+            ProductDelegate productDelegate = answer.GetProduct;
+            double productResult = productDelegate(num1, num2);
+            Console.WriteLine($" {num1} * {num2} = {productResult}");
+
+            //action delegate for GetSmaller method
+            Action<double, double> smallerAction = answer.GetSmaller;
+            smallerAction(num1, num2);
         }
     }
 }
