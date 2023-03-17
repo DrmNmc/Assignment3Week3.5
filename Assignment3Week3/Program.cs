6d04138f106ff1c6a070b518300483ee9045dd58
Dylan <mill1429@mail.nmc.edu>
2023-03-17 01:57:13 -0400

    init

--- Assignment3Week3/Program.cs
+++ Assignment3Week3/Program.cs
@@ -1,79 +1,111 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace delegatesAndEvents
+namespace LinqQueries
 {
-    public delegate void RaceWinnerDelegate(int winner);
-
-    public class Race
+    class FamousPeople
     {
-        //delegate event obj
-        public event RaceWinnerDelegate RaceWinnerEvent;
-
-        public void Racing(int contestants, int laps)
-        {
-            Console.WriteLine("Ready\nSet\nGo!");
-            Random r = new Random();
-            int[] participants = new int[contestants];
-            bool done = false;
-            int champ = -1;
-
-            while (!done)
-            {
-                for (int i = 0; i < contestants; i++)
-                {
-                    if (participants[i] <= laps)
-                    {
-                        participants[i] += r.Next(1, 5);
-                    }
-                    else
-                    {
-                        champ = i;
-                        done = true;
-                        continue;
-                    }
-                }
-            }
-
-            TheWinner(champ);
-        }
-
-        private void TheWinner(int champ)
-        {
-            Console.WriteLine("We have a winner!");
-            //invoking event obj
-            RaceWinnerEvent?.Invoke(champ);
-        }
+        public string Name { get; set; }
+        public int? BirthYear { get; set; } = null;
+        public int? DeathYear { get; set; } = null;
     }
 
     class Program
     {
-        public static void Main()
+        static void Main(string[] args)
         {
-            Race round1 = new Race();
 
-            round1.RaceWinnerEvent += footRace;
+        IList<FamousPeople> stemPeople = new List<FamousPeople>() {
+                new FamousPeople() { Name= "Michael Faraday", BirthYear=1791,DeathYear=1867 },
+                new FamousPeople() { Name= "James Clerk Maxwell", BirthYear=1831,DeathYear=1879 },
+                new FamousPeople() { Name= "Marie Skłodowska Curie", BirthYear=1867,DeathYear=1934 },
+                new FamousPeople() { Name= "Katherine Johnson", BirthYear=1918,DeathYear=2020 },
+                new FamousPeople() { Name= "Jane C. Wright", BirthYear=1919,DeathYear=2013 },
+                new FamousPeople() { Name = "Tu YouYou", BirthYear= 1930 },
+                new FamousPeople() { Name = "Françoise Barré-Sinoussi", BirthYear=1947 },
+                new FamousPeople() {Name = "Lydia Villa-Komaroff", BirthYear=1947},
+                new FamousPeople() {Name = "Mae C. Jemison", BirthYear=1956},
+                new FamousPeople() {Name = "Stephen Hawking", BirthYear=1942,DeathYear=2018 },
+                new FamousPeople() {Name = "Tim Berners-Lee", BirthYear=1955 },
+                new FamousPeople() {Name = "Terence Tao", BirthYear=1975 },
+                new FamousPeople() {Name = "Florence Nightingale", BirthYear=1820,DeathYear=1910 },
+                new FamousPeople() {Name = "George Washington Carver", DeathYear=1943 },
+                new FamousPeople() {Name = "Frances Allen", BirthYear=1932,DeathYear=2020 },
+                new FamousPeople() {Name = "Bill Gates", BirthYear=1955 }
+     };
 
-            round1.Racing(5, 50);
+        //birthdates after 1900
+        var query1 = from person in stemPeople
+                         where person.BirthYear > 1900
+                         select person;
 
-            round1.RaceWinnerEvent -= footRace;
-            round1.RaceWinnerEvent += carRace;
+            Console.WriteLine("People with birthdates after 1900:");
+            foreach (var person in query1)
+            {
+                Console.WriteLine($"{person.Name}");
+            }
+            Console.WriteLine();
 
-            round1.Racing(5, 50);
+            //two lowercase L's in their name
+            var query2 = from person in stemPeople
+                         where person.Name.Count(c => c == 'l') == 2
+                         select person;
 
-            //bike race event using lambda
-            round1.RaceWinnerEvent -= carRace;
-            round1.RaceWinnerEvent += (int winner) => { Console.WriteLine($"Biker number {winner} is the winner."); };
+            Console.WriteLine("People with two lowercase L's in their name:");
+            foreach (var person in query2)
+            {
+                Console.WriteLine($"{person.Name}");
+            }
+            Console.WriteLine();
 
-            round1.Racing(5, 50);
-        }
+            //people with birthdays after 1950
+            var query3 = from person in stemPeople
+                         where person.BirthYear > 1950
+                         select person;
 
-        public static void carRace(int winner)
-        {
-            Console.WriteLine($"Car number {winner} is the winner.");
-        }
-        public static void footRace(int winner)
-        {
-            Console.WriteLine($"Racer number {winner} is the winner.");
+            int countAfter1950 = query3.Count();
+            Console.WriteLine($"Number of people with birthdays after 1950: {countAfter1950}");
+            Console.WriteLine();
+
+            //birth years between 1920 and 2000; name and count.
+            var query4 = from person in stemPeople
+                         where person.BirthYear >= 1920 && person.BirthYear <= 2000
+                         select person;
+
+            Console.WriteLine("People with birth years between 1920 and 2000:");
+            foreach (var person in query4)
+            {
+                Console.WriteLine($"{person.Name}");
+            }
+            int birthCount = query4.Count();
+            Console.WriteLine($"Count of people with birth years between 1920 and 2000: {birthCount}");
+            Console.WriteLine();
+
+            //sort by birthyear
+            var query5 = from person in stemPeople
+                         orderby person.BirthYear
+                         select person;
+
+            Console.WriteLine("Sorted list by BirthYear:");
+            foreach (var person in query5)
+            {
+                Console.WriteLine($"{person.Name} - {person.BirthYear}");
+            }
+            Console.WriteLine();
+
+            //sort death year 1960-2015 in asc order.
+            var query6 = from person in stemPeople
+                         where person.DeathYear > 1960 && person.DeathYear < 2015
+                         orderby person.Name ascending
+                         select person;
+
+            Console.WriteLine("People with a death year after 1960 and before 2015 (sorted by name):");
+            foreach (var person in query6)
+            {
+                Console.WriteLine($"{person.Name}");
+            }
+            Console.WriteLine();
         }
     }
 }
