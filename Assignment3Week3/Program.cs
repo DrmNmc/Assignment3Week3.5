b95c698d332eb957cede1325bdac3184545327b9
Dylan <mill1429@mail.nmc.edu>
2023-03-17 02:09:24 -0400

    init

--- Assignment3Week3/Program.cs
+++ Assignment3Week3/Program.cs
@@ -2,110 +2,130 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace LinqQueries
+namespace LinqMethodSyntax
 {
-    class FamousPeople
+    public class Student
     {
-        public string Name { get; set; }
-        public int? BirthYear { get; set; } = null;
-        public int? DeathYear { get; set; } = null;
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+        public string Major { get; set; }
+        public double Tuition { get; set; }
+    }
+
+    public class StudentClubs
+    {
+        public int StudentID { get; set; }
+        public string ClubName { get; set; }
+    }
+
+    public class StudentGPA
+    {
+        public int StudentID { get; set; }
+        public double GPA { get; set; }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
+            // Student collection
+            IList<Student> studentList = new List<Student>() {
+                new Student() { StudentID = 1, StudentName = "Frank Furter", Age = 55, Major="Hospitality", Tuition=3500.00} ,
+                new Student() { StudentID = 1, StudentName = "Gina Host", Age = 21, Major="Hospitality", Tuition=4500.00 } ,
+                new Student() { StudentID = 2, StudentName = "Cookie Crumb",  Age = 21, Major="CIT", Tuition=2500.00 } ,
+                new Student() { StudentID = 3, StudentName = "Ima Script",  Age = 48, Major="CIT", Tuition=5500.00 } ,
+                new Student() { StudentID = 3, StudentName = "Cora Coder",  Age = 35, Major="CIT", Tuition=1500.00 } ,
+                new Student() { StudentID = 4, StudentName = "Ura Goodchild" , Age = 40, Major="Marketing", Tuition=500.00} ,
+                new Student() { StudentID = 5, StudentName = "Take Mewith" , Age = 29, Major="Aerospace Engineering", Tuition=5500.00 }
+            };
 
-        IList<FamousPeople> stemPeople = new List<FamousPeople>() {
-                new FamousPeople() { Name= "Michael Faraday", BirthYear=1791,DeathYear=1867 },
-                new FamousPeople() { Name= "James Clerk Maxwell", BirthYear=1831,DeathYear=1879 },
-                new FamousPeople() { Name= "Marie Skłodowska Curie", BirthYear=1867,DeathYear=1934 },
-                new FamousPeople() { Name= "Katherine Johnson", BirthYear=1918,DeathYear=2020 },
-                new FamousPeople() { Name= "Jane C. Wright", BirthYear=1919,DeathYear=2013 },
-                new FamousPeople() { Name = "Tu YouYou", BirthYear= 1930 },
-                new FamousPeople() { Name = "Françoise Barré-Sinoussi", BirthYear=1947 },
-                new FamousPeople() {Name = "Lydia Villa-Komaroff", BirthYear=1947},
-                new FamousPeople() {Name = "Mae C. Jemison", BirthYear=1956},
-                new FamousPeople() {Name = "Stephen Hawking", BirthYear=1942,DeathYear=2018 },
-                new FamousPeople() {Name = "Tim Berners-Lee", BirthYear=1955 },
-                new FamousPeople() {Name = "Terence Tao", BirthYear=1975 },
-                new FamousPeople() {Name = "Florence Nightingale", BirthYear=1820,DeathYear=1910 },
-                new FamousPeople() {Name = "George Washington Carver", DeathYear=1943 },
-                new FamousPeople() {Name = "Frances Allen", BirthYear=1932,DeathYear=2020 },
-                new FamousPeople() {Name = "Bill Gates", BirthYear=1955 }
-     };
+            // Student GPA Collection
+            IList<StudentGPA> studentGPAList = new List<StudentGPA>() {
+                new StudentGPA() { StudentID = 1,  GPA=4.0} ,
+                new StudentGPA() { StudentID = 2,  GPA=3.5} ,
+                new StudentGPA() { StudentID = 3,  GPA=2.0 } ,
+                new StudentGPA() { StudentID = 4,  GPA=1.5 } ,
+                new StudentGPA() { StudentID = 5,  GPA=4.0 } ,
+                new StudentGPA() { StudentID = 6,  GPA=2.5} ,
+                new StudentGPA() { StudentID = 7,  GPA=1.0 }
+            };
+            // Club collection
+                IList<StudentClubs> studentClubList = new List<StudentClubs>() {
+                new StudentClubs() {StudentID=1, ClubName="Photography" },
+                new StudentClubs() {StudentID=1, ClubName="Game" },
+                new StudentClubs() {StudentID=2, ClubName="Game" },
+                new StudentClubs() {StudentID=5, ClubName="Photography" },
+                new StudentClubs() {StudentID=6, ClubName="Game" },
+                new StudentClubs() {StudentID=7, ClubName="Photography" },
+                new StudentClubs() {StudentID=3, ClubName="PTK" },
+            };
 
-        //birthdates after 1900
-        var query1 = from person in stemPeople
-                         where person.BirthYear > 1900
-                         select person;
-
-            Console.WriteLine("People with birthdates after 1900:");
-            foreach (var person in query1)
+            //group by GPA. display ID.
+            var queryA = studentGPAList.GroupBy(s => s.GPA);
+            Console.WriteLine("Students grouped by GPA:");
+            foreach (var group in queryA)
             {
-                Console.WriteLine($"{person.Name}");
+                Console.WriteLine($"GPA: {group.Key}");
+                foreach (var student in group)
+                {
+                    Console.WriteLine($"StudentID: {student.StudentID}");
+                }
             }
             Console.WriteLine();
 
-            //two lowercase L's in their name
-            var query2 = from person in stemPeople
-                         where person.Name.Count(c => c == 'l') == 2
-                         select person;
-
-            Console.WriteLine("People with two lowercase L's in their name:");
-            foreach (var person in query2)
+            //sort club. group club. display id.
+            var queryB = studentClubList.OrderBy(s => s.ClubName).GroupBy(s => s.ClubName);
+            Console.WriteLine("Students sorted by club and grouped by club:");
+            foreach (var group in queryB)
             {
-                Console.WriteLine($"{person.Name}");
+                Console.WriteLine($"Club: {group.Key}");
+                foreach (var student in group)
+                {
+                    Console.WriteLine($"StudentID: {student.StudentID}");
+                }
             }
             Console.WriteLine();
 
-            //people with birthdays after 1950
-            var query3 = from person in stemPeople
-                         where person.BirthYear > 1950
-                         select person;
-
-            int countAfter1950 = query3.Count();
-            Console.WriteLine($"Number of people with birthdays after 1950: {countAfter1950}");
+            //num of students between 2.5-4.0 GPA
+            var queryC = studentGPAList.Count(s => s.GPA >= 2.5 && s.GPA <= 4.0);
+            Console.WriteLine($"Number of students with a GPA between 2.5 and 4.0: {queryC}");
             Console.WriteLine();
 
-            //birth years between 1920 and 2000; name and count.
-            var query4 = from person in stemPeople
-                         where person.BirthYear >= 1920 && person.BirthYear <= 2000
-                         select person;
-
-            Console.WriteLine("People with birth years between 1920 and 2000:");
-            foreach (var person in query4)
-            {
-                Console.WriteLine($"{person.Name}");
-            }
-            int birthCount = query4.Count();
-            Console.WriteLine($"Count of people with birth years between 1920 and 2000: {birthCount}");
+            //Average student tuition
+            var queryD = studentList.Average(s => s.Tuition);
+            Console.WriteLine($"Average tuition: {queryD}");
             Console.WriteLine();
 
-            //sort by birthyear
-            var query5 = from person in stemPeople
-                         orderby person.BirthYear
-                         select person;
-
-            Console.WriteLine("Sorted list by BirthYear:");
-            foreach (var person in query5)
+            //student paying the most tuition. display name, major and tuition, shame them thoroughly!
+            var maxTuition = studentList.Max(s => s.Tuition);
+            var queryE = studentList.Where(s => s.Tuition == maxTuition);
+            Console.WriteLine("Student(s) paying the most tuition:");
+            foreach (var student in queryE)
             {
-                Console.WriteLine($"{person.Name} - {person.BirthYear}");
+                Console.WriteLine($"Name: {student.StudentName}, Major: {student.Major}, Tuition: {student.Tuition}");
             }
             Console.WriteLine();
 
-            //sort death year 1960-2015 in asc order.
-            var query6 = from person in stemPeople
-                         where person.DeathYear > 1960 && person.DeathYear < 2015
-                         orderby person.Name ascending
-                         select person;
-
-            Console.WriteLine("People with a death year after 1960 and before 2015 (sorted by name):");
-            foreach (var person in query6)
+            //join student list and GPA list on student ID and display the student's name, major, gpa
+            var queryF = studentList.Join(studentGPAList, s => s.StudentID, gpa => gpa.StudentID, (s, gpa) => new { s.StudentName, s.Major, gpa.GPA });
+            Console.WriteLine("Students' names, majors, and GPAs:");
+            foreach (var student in queryF)
             {
-                Console.WriteLine($"{person.Name}");
+                Console.WriteLine($"Name: {student.StudentName}, Major: {student.Major}, GPA: {student.GPA}");
+            }
+            Console.WriteLine();
+
+            //join student list and club list. Display the names game club students.
+            var queryG = studentList.Join(studentClubList, s => s.StudentID, club => club.StudentID, (s, club) => new { s.StudentName, club.ClubName })
+                                    .Where(sc => sc.ClubName == "Game");
+            Console.WriteLine("Nerds in the Game club:");
+            foreach (var student in queryG)
+            {
+                Console.WriteLine($"Name: {student.StudentName}");
             }
             Console.WriteLine();
         }
     }
 }
+
